2e087be3ebcd3457ca2765a2ab0ff7ffc0124079
rolisanchez <roli.s91@gmail.com>
2020-05-01 16:16:04 +0800

    Change gitignore and add initial project

---
+++ SwiftUIAuroraAmplifyTutorial/SwiftUIAuroraAmplifyTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAuroraAmplifyTutorial
+//
+//  Created by Victor Rolando Sanchez Jara on 5/1/20.
+//  Copyright © 2020 Victor Rolando Sanchez Jara. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
